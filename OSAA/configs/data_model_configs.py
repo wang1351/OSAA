2c8b73b6076caf5f6c0818947b191dc4bd0a0303
Wang Ziyan <50859355+wang1351@users.noreply.github.com>
2024-07-29 15:00:16 +0800

    add cwru

--- OSAA/configs/data_model_configs.py
+++ OSAA/configs/data_model_configs.py
@@ -27,4 +27,29 @@ class PU():
         self.disc_hid_dim = 64
         self.hidden_dim = 500
         self.DSKN_disc_hid = 128
+        self.decodersize = 640
+
+class CWRU():
+    def __init__(self):
+        super(CWRU, self)
+        self.scenarios =[("FE1772","DE1797"),("DE1772", "FE1797"), ("FE1730", "DE1750")]
+        self.class_names = ['healthy','IF','OF','B']
+        self.sequence_len = 1024
+        self.shuffle = True
+        self.drop_last = True
+        self.normalize = True
+
+        self.input_channels = 1
+        self.kernel_size = 9
+        self.stride = 1
+        self.dropout = 0.4
+        self.num_classes = 4
+
+        self.mid_channels = 64
+        self.final_out_channels = 128
+        self.features_len = 1
+        self.disc_hid_dim = 64
+        self.hidden_dim = 500
+        self.DSKN_disc_hid = 128
+        self.decodersize = 128
 
