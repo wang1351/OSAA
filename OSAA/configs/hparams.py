296d8345c58927340961880e9f1dc6edd8775ac9
Wang Ziyan <50859355+wang1351@users.noreply.github.com>
2024-07-29 15:01:02 +0800

    add cwru

--- OSAA/configs/hparams.py
+++ OSAA/configs/hparams.py
@@ -8,12 +8,24 @@ class PU():
     def __init__(self):
         super(PU, self).__init__()
         self.train_params = {
-                'num_epochs': 20,
+                'num_epochs': 1,
                 'batch_size': 64,
                 'weight_decay': 1e-5
         }
         self.alg_hparams = {
             'DISTANT':{'learning_rate': 1e-4,   'clasweight':1,  'domainweight': 1,    'threshold_s':0.5,  'threshold_i': 0.5}}
+
+class CWRU():
+    def __init__(self):
+        super(CWRU, self).__init__()
+        self.train_params = {
+                'num_epochs': 5,
+                'batch_size': 64,
+                'weight_decay': 1e-5
+        }
+        self.alg_hparams = {
+
+            'DISTANT':{'learning_rate': 1e-4,   'clasweight':1,  'domainweight': 0.3,    'threshold_s':0.5,  'threshold_i': 0.5}}
         
 
 
